12eb192867446d4cea6bf60a70de0c3a09aa3b02
jayesh-zip <patiljayesh1469@gmail.com>
2024-10-05 09:08:09 +0530

    create more chat api like addmember, removememebr, leavegroup etc.., first commit of today, work in progress.

--- server/routes/chat.js
+++ server/routes/chat.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { isAuthenticated } from "../middlewares/auth.js";
-import { getMyChats, getMyGroups, newGroupChat } from "../controllers/chat.js";
+import { addMembers, getMyChats, getMyGroups, leaveGroup, newGroupChat, removeMember } from "../controllers/chat.js";
 
 const app = express.Router();
 
@@ -13,5 +13,13 @@ app.post("/new", newGroupChat);
 app.get("/my", getMyChats);
 
 app.get("/my/groups", getMyGroups);
+
+
+app.put("/addMembers", addMembers);
+
+app.put("/removemember", removeMember);
+
+
+app.delete("/leave/:id", leaveGroup);
 
 export default app;
