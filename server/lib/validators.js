b449abf94afd8ee6c3d16a2ab2df39c94dcf9599
jayesh-zip <patiljayesh1469@gmail.com>
2024-10-08 09:37:49 +0530

    create registerValidator for validate user input data, work in progress.

---
+++ server/lib/validators.js
@@ -0,0 +1,24 @@
+import { body, param, validationResult } from "express-validator";
+import { ErrorHandler } from "../utils/utility.js";
+
+const validateHandler = (req, res, next) => {
+    const errors = validationResult(req);
+  
+    const errorMessages = errors
+      .array()
+      .map((error) => error.msg)
+      .join(", ");
+  
+    if (errors.isEmpty()) return next();
+    else next(new ErrorHandler(errorMessages, 400));
+  };
+  
+  const registerValidator = () => [
+    body("name", "Please Enter Name").notEmpty(),
+    body("username", "Please Enter Username").notEmpty(),
+    body("bio", "Please Enter Bio").notEmpty(),
+    body("password", "Please Enter Password").notEmpty(),
+  ];
+
+
+export { validateHandler, registerValidator};
--- server/routes/user.js
+++ server/routes/user.js
@@ -2,10 +2,11 @@ import express from "express";
 import {getMyProfile, login, logout, newUser} from "../controllers/user.js";
 import { singleAvatar } from "../middlewares/multer.js";
 import { isAuthenticated } from "../middlewares/auth.js";
+import { registerValidator, validateHandler } from "../lib/validators.js";
 
 const app = express.Router();
 
-app.post("/new", singleAvatar, newUser);
+app.post("/new", singleAvatar,  registerValidator(), validateHandler, newUser);
 
 app.get("/login", login);
 
