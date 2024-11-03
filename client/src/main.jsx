cdfe3ebafa7dd559ca77761c032e6fba461c1002
jayesh-zip <patiljayesh1469@gmail.com>
2024-11-03 19:37:49 +0530

    Integrate Redux provider with store in main render function

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,16 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { CssBaseline } from "@mui/material";
-import { HelmetProvider } from "react-helmet-async"; // Keep this if needed for managing head elements (e.g., title, meta tags)
+import { HelmetProvider } from "react-helmet-async";
+import { Provider } from "react-redux";
+import store from "./redux/store.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    {/* Removed Redux store as it's not needed for frontend-only */}
-    <HelmetProvider>
-      <CssBaseline />
-      <div onContextMenu={(e) => e.preventDefault()}>
-        <App />
-      </div>
-    </HelmetProvider>
+    <Provider store={store}>
+      <HelmetProvider>
+        <CssBaseline />
+        <div onContextMenu={(e) => e.preventDefault()}>
+          <App />
+        </div>
+      </HelmetProvider>
+    </Provider>
   </React.StrictMode>
 );
