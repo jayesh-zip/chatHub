27442a87e2f57fb75afa55de5eb800b7b87fca3b
Jayesh Patil <132203403+jayesh-zip@users.noreply.github.com>
2024-11-05 22:19:13 +0530

    Update Chat.jsx

--- client/src/pages/Chat.jsx
+++ client/src/pages/Chat.jsx
@@ -6,6 +6,7 @@ import {
   AttachFile as AttachFileIcon,
   Send as SendIcon,
 } from "@mui/icons-material";
+
 import { InputBox } from "../components/styles/StyledComponents";
 import FileMenu from "../components/dialogs/FileMenu";
 import MessageComponent from "../components/shared/MessageComponent";
