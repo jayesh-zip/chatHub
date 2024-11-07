79c13b17077a43dcd7ae75ce0fbd62db920aae7a
jayesh-zip <patiljayesh1469@gmail.com>
2024-11-07 08:20:59 +0530

    Add ChatList with alerts and online status

--- client/src/components/specific/ChatList.jsx
+++ client/src/components/specific/ChatList.jsx
@@ -1,8 +1,10 @@
 import { Stack } from "@mui/material";
+import React from "react";
+import ChatItem from "../shared/ChatItem";
 
 const ChatList = ({
   w = "100%",
-  chats = [1,2,3,4],
+  chats = [],
   chatId,
   onlineUsers = [],
   newMessagesAlert = [
@@ -15,9 +17,30 @@ const ChatList = ({
 }) => {
   return (
     <Stack width={w} direction={"column"} overflow={"auto"} height={"100%"}>
-      {chats?.map((data) => {
+      {chats?.map((data, index) => {
+        const { avatar, _id, name, groupChat, members } = data;
+
+        const newMessageAlert = newMessagesAlert.find(
+          ({ chatId }) => chatId === _id
+        );
+
+        const isOnline = members?.some((member) =>
+          onlineUsers.includes(member)
+        );
+
         return (
-          <div>{data}</div>
+          <ChatItem
+            index={index}
+            newMessageAlert={newMessageAlert}
+            isOnline={isOnline}
+            avatar={avatar}
+            name={name}
+            _id={_id}
+            key={_id}
+            groupChat={groupChat}
+            sameSender={chatId === _id}
+            handleDeleteChat={handleDeleteChat}
+          />
         );
       })}
     </Stack>
