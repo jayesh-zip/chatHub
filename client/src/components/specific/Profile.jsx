6a900a9a5ba0de1da202135b1edb5c0f1ed58105
jayesh-zip <patiljayesh1469@gmail.com>
2024-11-07 08:23:32 +0530

    Add user profile display with bio and joined date

--- client/src/components/specific/Profile.jsx
+++ client/src/components/specific/Profile.jsx
@@ -1,9 +1,59 @@
-import React from 'react'
+import React from "react";
+import { Avatar, Stack, Typography } from "@mui/material";
+import {
+  Face as FaceIcon,
+  AlternateEmail as UserNameIcon,
+  CalendarMonth as CalendarIcon,
+} from "@mui/icons-material";
+import moment from "moment";
+import { transformImage } from "../../lib/features";
 
-function Profile() {
+const Profile = ({ user }) => {
   return (
-    <div>Profile</div>
-  )
-}
+    <Stack spacing={"2rem"} direction={"column"} alignItems={"center"}>
+      <Avatar
+        src={transformImage(user?.avatar?.url)}
+        sx={{
+          width: 200,
+          height: 200,
+          objectFit: "contain",
+          marginBottom: "1rem",
+          border: "5px solid white",
+        }}
+      />
+      <ProfileCard heading={"Bio"} text={user?.bio} />
+      <ProfileCard
+        heading={"Username"}
+        text={user?.username}
+        Icon={<UserNameIcon />}
+      />
+      <ProfileCard heading={"Name"} text={user?.name} Icon={<FaceIcon />} />
+      <ProfileCard
+        heading={"Joined"}
+        text={moment(user?.createdAt).fromNow()}
+        Icon={<CalendarIcon />}
+      />
+    </Stack>
+  );
+};
 
-export default Profile
+const ProfileCard = ({ text, Icon, heading }) => (
+  <Stack
+    direction={"row"}
+    alignItems={"center"}
+    spacing={"1rem"}
+    color={"white"}
+    textAlign={"center"}
+  >
+    {Icon && Icon}
+
+    <Stack>
+      <Typography variant="body1">{text}</Typography>
+      <Typography color={"gray"} variant="caption">
+        {heading}
+      </Typography>
+    </Stack>
+  </Stack>
+);
+
+export default Profile;
