b20dbf7b73753e6fbc231c4b3f3e477a29c95d0d
jayesh-zip <patiljayesh1469@gmail.com>
2024-11-08 09:49:05 +0530

    Implement user management UI with table and avatar display

---
+++ client/src/pages/admin/UserManagement.jsx
@@ -0,0 +1,56 @@
+import { Avatar, Skeleton } from "@mui/material";
+import React from "react";
+import AdminLayout from "../../components/layout/AdminLayout";
+import Table from "../../components/shared/Table";
+
+const columns = [
+  {
+    field: "id",
+    headerName: "ID",
+    headerClassName: "table-header",
+    width: 200,
+  },
+  {
+    field: "avatar",
+    headerName: "Avatar",
+    headerClassName: "table-header",
+    width: 150,
+    renderCell: (params) => (
+      <Avatar alt={params.row.name} src={params.row.avatar} />
+    ),
+  },
+
+  {
+    field: "name",
+    headerName: "Name",
+    headerClassName: "table-header",
+    width: 200,
+  },
+  {
+    field: "username",
+    headerName: "Username",
+    headerClassName: "table-header",
+    width: 200,
+  },
+  {
+    field: "friends",
+    headerName: "Friends",
+    headerClassName: "table-header",
+    width: 150,
+  },
+  {
+    field: "groups",
+    headerName: "Groups",
+    headerClassName: "table-header",
+    width: 200,
+  },
+];
+
+const UserManagement = () => (
+  <AdminLayout>
+    <Skeleton height={"100vh"} />
+    <Table heading={"All Users"} columns={columns} rows={[]} />
+  </AdminLayout>
+);
+
+export default UserManagement;
