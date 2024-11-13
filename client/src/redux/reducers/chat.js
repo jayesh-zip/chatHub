824500fa8d7e16521861d6a8b422f371a130d74a
jayesh-zip <patiljayesh1469@gmail.com>
2024-11-13 15:54:11 +0530

    Add new messages alert tracking by chat ID in chat slice

--- client/src/redux/reducers/chat.js
+++ client/src/redux/reducers/chat.js
@@ -25,11 +25,29 @@ const chatSlice = createSlice({
     resetNotificationCount: (state) => {
       state.notificationCount = 0;
     },
+
+    setNewMessagesAlert: (state, action) => {
+      const chatId = action.payload.chatId;
+
+      const index = state.newMessagesAlert.findIndex(
+        (item) => item.chatId === chatId
+      );
+
+      if (index !== -1) {
+        state.newMessagesAlert[index].count += 1;
+      } else {
+        state.newMessagesAlert.push({
+          chatId,
+          count: 1,
+        });
+      }
+    },
   },
 });
 
 export default chatSlice;
 export const {
   incrementNotification,
-  resetNotificationCount
+  resetNotificationCount,
+  setNewMessagesAlert,
 } = chatSlice.actions;
