60dc70952ad0cf3c08160e18323ac682da45212d
jayesh-zip <patiljayesh1469@gmail.com>
2024-11-25 11:40:48 +0530

    Refactor: Reformat code for improved readability and consistency

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -1,5 +1,13 @@
 import express from "express";
-import {adminLogin, adminLogout, allChats, allMessages, allUsers, getAdminData, getDashboardStats} from "../controllers/admin.js";
+import {
+  adminLogin,
+  adminLogout,
+  allChats,
+  allMessages,
+  allUsers,
+  getAdminData,
+  getDashboardStats,
+} from "../controllers/admin.js";
 import { adminLoginValidator, validateHandler } from "../lib/validators.js";
 import { adminOnly } from "../middlewares/auth.js";
 
@@ -10,6 +18,7 @@ app.post("/verify", adminLoginValidator(), validateHandler, adminLogin);
 app.get("/logout", adminLogout);
 
 // Only Admin Can Accecss these Routes
+
 app.use(adminOnly);
 
 app.get("/", getAdminData);
