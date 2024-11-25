f75305b597274c572de1a6539b9faff4054d9a86
jayesh-zip <patiljayesh1469@gmail.com>
2024-11-25 11:42:47 +0530

    Refactor: Change login route to POST and reorder imports

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,15 +1,29 @@
 import express from "express";
-import {acceptFriendRequest, getMyFriends, getMyNotifications, getMyProfile, login, logout, newUser, searchUser, sendFriendRequest} from "../controllers/user.js";
-import { singleAvatar } from "../middlewares/multer.js";
+import {
+  acceptFriendRequest,
+  getMyFriends,
+  getMyNotifications,
+  getMyProfile,
+  login,
+  logout,
+  newUser,
+  searchUser,
+  sendFriendRequest,
+} from "../controllers/user.js";
+import {
+  acceptRequestValidator,
+  loginValidator,
+  registerValidator,
+  sendRequestValidator,
+  validateHandler,
+} from "../lib/validators.js";
 import { isAuthenticated } from "../middlewares/auth.js";
-import { acceptRequestValidator, loginValidator, registerValidator, sendRequestValidator, validateHandler } from "../lib/validators.js";
+import { singleAvatar } from "../middlewares/multer.js";
 
 const app = express.Router();
 
-app.post("/new", singleAvatar,  registerValidator(), validateHandler, newUser);
-
-app.get("/login",loginValidator(), validateHandler, login);
-
+app.post("/new", singleAvatar, registerValidator(), validateHandler, newUser);
+app.post("/login", loginValidator(), validateHandler, login);
 
 // After here user must be logged in to access the routes
 
@@ -17,13 +31,23 @@ app.use(isAuthenticated);
 
 app.get("/me", getMyProfile);
 
-app.get("/search", searchUser);
-
 app.get("/logout", logout);
 
-app.put("/sendrequest", sendRequestValidator(), validateHandler, sendFriendRequest );
+app.get("/search", searchUser);
 
-app.put("/acceptrequest",acceptRequestValidator(),validateHandler,acceptFriendRequest);
+app.put(
+  "/sendrequest",
+  sendRequestValidator(),
+  validateHandler,
+  sendFriendRequest
+);
+
+app.put(
+  "/acceptrequest",
+  acceptRequestValidator(),
+  validateHandler,
+  acceptFriendRequest
+);
 
 app.get("/notifications", getMyNotifications);
 
